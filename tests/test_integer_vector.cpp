9f054697cfd20d8cddf1a82613ff37bb6bcd32dd
Oleksandr Torba <aleks.torba20@gmail.com>
2024-08-21 19:35:56 +0200

    Add copy constructor and assignment operator tests for IntegerVector

--- tests/test_integer_vector.cpp
+++ tests/test_integer_vector.cpp
@@ -17,6 +17,31 @@ TEST(IntegerVectorTest, ParameterizedConstructor)
 	EXPECT_TRUE(vec.empty());
 }
 
+// Test the copy constructor
+TEST(IntegerVectorTest, CopyConstructor)
+{
+	IntegerVector vec1;
+	vec1.push_back(1);
+	vec1.push_back(2);
+	IntegerVector vec2(vec1);
+	EXPECT_EQ(vec2.size(), 2);
+	EXPECT_EQ(vec2.at(0), 1);
+	EXPECT_EQ(vec2.at(1), 2);
+}
+
+// Test the assignment operator
+TEST(IntegerVectorTest, AssignmentOperator)
+{
+	IntegerVector vec1;
+	vec1.push_back(1);
+	vec1.push_back(2);
+	IntegerVector vec2;
+	vec2 = vec1;
+	EXPECT_EQ(vec2.size(), 2);
+	EXPECT_EQ(vec2.at(0), 1);
+	EXPECT_EQ(vec2.at(1), 2);
+}
+
 // Test push_back and size
 TEST(IntegerVectorTest, PushBackAndSize)
 {
@@ -48,6 +73,20 @@ TEST(IntegerVectorTest, PopBack)
 	vec.pop_back();
 	EXPECT_EQ(vec.size(), 1);
 	EXPECT_EQ(vec.at(0), 1);
+}
+
+// Test print
+TEST(IntegerVectorTest, Print)
+{
+	IntegerVector vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	std::ostringstream oss;
+	std::streambuf *p_cout_streambuf = std::cout.rdbuf();
+	std::cout.rdbuf(oss.rdbuf());
+	vec.print();
+	std::cout.rdbuf(p_cout_streambuf);
+	EXPECT_EQ(oss.str(), "1 2 \n");
 }
 
 // Test shrinking
